3f05e7675a7efd162bff60da60c5503e3d3765fb
by77er <by77er@gmail.com>
2023-09-24 16:18:34 -0400

    Update with new font and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,16 @@
-import { Fira_Sans } from '@next/font/google'
 import styles from './page.module.css'
-
-const fira = Fira_Sans({ subsets: ['latin'], weight: "400" });
 
 export default function Home() {
   return (
     <main>
       <div className={styles.heroContainer}>
-        <img src="/tfc_logo.png" alt="TFC Logo" className={styles.heroImage} />
-        <h1 className={`${fira.className} ${styles.heroTitle}`}>Tampa Furry Club</h1>
-        <p className={`${fira.className} ${styles.heroDesc}`}>
-          Looking to meet furries in Tampa, FL? Here we are.
+        <img src="/tfc_logo.png" alt="Tampa Furry Club Logo" className={styles.heroImage} />
+        <p className={`${styles.heroDesc}`}>
+          Looking for furries in Tampa?
         </p>
-        <div className={styles.arrowDown}></div>
         <a href="https://discord.gg/vq2q3qnynG">
-          <button className={`${fira.className} ${styles.discordButton}`}>
-            Join on Discord
+          <button className={`${styles.discordButton}`}>
+            Join Our Discord!
             <img src="/discord.png" alt="" className={styles.discordIcon}></img>
           </button>
         </a>
